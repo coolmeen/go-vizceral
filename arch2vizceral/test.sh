c0284d6b78cb2dbb9cc3f1b839083263c4d4cbf9
Jorge Quilcate Otoya <quilcate.jorge@gmail.com>
2018-02-28 10:48:20 +0100

    tweets demo example

--- arch2vizceral/test.sh
+++ arch2vizceral/test.sh
@@ -1,3 +1,3 @@
-./arch2vizceral -arch edge | python -m json.tool  > edge_vizceral.json
-./arch2vizceral -arch test | python -m json.tool  > test_vizceral.json
+./arch2vizceral -arch tweets_service_v2 | python -m json.tool  > tweets_vizceral.json
+#./arch2vizceral -arch test | python -m json.tool  > test_vizceral.json
 cp *.json ../../vizceral-example/src
